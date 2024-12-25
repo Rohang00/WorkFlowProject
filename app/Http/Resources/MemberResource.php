53b57b38971d56948dadd410821a67bec177e51a
ShivahariRukesh <478gnujnoej@gmail.com>
2024-12-25 11:03:38 +0545

    Created the member crud api

---
+++ app/Http/Resources/MemberResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class MemberResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        // return parent::toArray($request);
+
+        return [
+            "id"=>$this->id,
+            "role"=>$this->role,
+            "status"=>$this->status,
+            "user_id"=>$this->user_id,
+            "org_id"=>$this->org_id,
+        ];
+    }
+}
